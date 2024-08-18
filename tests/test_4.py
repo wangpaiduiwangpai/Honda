a15acf3769c2df28358d364a248a3798884997ee
QBZ03TY <yhy66619212023@outlook.com>
2024-08-18 12:51:08 +0800

    rgb

---
+++ tests/test_4.py
@@ -0,0 +1,30 @@
+def test_add():
+    assert 2 + 2 == 4
+    
+
+def test_subtract():
+    assert 4 - 2 == 2
+    
+
+def test_multiply():
+    assert 2 * 2 == 4
+    
+
+def test_divide():
+    assert 4 / 2 == 2
+    
+
+def test_power():
+    assert 2 ** 2 == 4
+    
+
+def test_modulo():
+    assert 5 % 2 == 1
+    
+
+def test_floor_division():
+    assert 5 // 2 == 2
+    
+
+def test_bitwise_and():
+    assert 5 & 3 == 1
